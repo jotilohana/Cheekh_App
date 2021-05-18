554565c1fa2e011f30c2eef50ed4e523f194d2ff
jotilohana <jklohana208@gmail.com>
2021-05-18 19:47:54 +0500

    'Csschanges'

--- src/screens/OnBoardingScreen.js
+++ src/screens/OnBoardingScreen.js
@@ -10,20 +10,20 @@ const OnBoardingScreen = (navigation) => {
                 {
                     backgroundColor: '#fff',
                     image: <Image style={styles.logo} source={require('../Images/image1.jpg')} />,
-                    title: 'How it Works',
-                    subtitle: '',
+                    title: <Text style={styles.lighttext}>How it Works</Text>,
+                    subtitle:<Text></Text>,
                 },
                 {
                     backgroundColor: '#fff',
                    image: <Image style={styles.logo}  source={require('../Images/image2.jpg')} />,
-                    title: <Text style={styles.title}>PRESS POWER{'\n'} 3/4 TIMES</Text>,
-                    subtitle: 'Guardian will receive SMS alerts with GEO location',
+                    title: <Text style={styles.lighttext}>PRESS POWER{'\n'} 3/4 TIMES</Text>,
+                    subtitle: <Text style={styles.subtext}></Text>'Guardian will receive SMS alerts with GEO location',
                 },
                 {
                     backgroundColor: '#fff',
                    image: <Image style={styles.logo}  source={require('../Images/image3.jpg')} />,
-                    title: 'WHEN \n TRIGGERED\n Your PHONE',
-                   subtitle: 'SMS with LOCATION \n Automatic place CALL',
+                   title: <View><Text style={styles.lighttext}>When</Text><Text style={styles.redtext}>{"\n"}Triggered</Text><Text style={styles.lighttext}>{"\n"}Your Phone</Text></View>,
+                   subtitle: <Text style={styles.subtext}>SMS with LOCATION \n Automatic place CALL</Text> ,
                 }, 
              ]}
                         />
@@ -31,18 +31,26 @@ const OnBoardingScreen = (navigation) => {
     );
 }
 
-const styles=StyleSheet.create({
-    logo:{
-        width:150,
-        height:150
+const styles = StyleSheet.create({
+    
+    logo: {
+      width: 200,
+      height: 200,
     },
-    title:{
+    lighttext:{
+        color:'#828080',
+        fontWeight:'bold',
         fontSize:20
+    },
+    redtext:{
+        color:'red',
+        fontWeight:'bold',
+        fontSize:40
+    },
+    subtext:{
+        color:'#828080',
+        fontSize:10
     }
-})
-
-
-
-
+  });
 
 export default OnBoardingScreen;
